84508c6fe2fa0515547b89ccedd74a5fbc50868d
mehidy <mehidy.gb@gmail.com>
2025-05-04 18:19:29 +0600

    Update NUXT_PORT in ecosystem configuration for staging environment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'nhl-staging-dashboard',
       script: '.output/server/index.mjs',
       env: {
-        NUXT_PORT: 3100,
+        NUXT_PORT: 3000,
         NUXT_HOST: '0.0.0.0',
         NODE_ENV: 'staging',
         NUXT_PUBLIC_API_URL: 'https://staging-replica-api.apexdrive365.com/api'
